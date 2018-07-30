bdc08d65526eb5f50a34979caa2832062f845567
lico <lico.atom@gmail.com>
2018-07-30 14:55:48 +0800

    initial commit with build script.

---
+++ README.md
@@ -0,0 +1,51 @@
+<p align="center">
+  <a href="/" rel="noopener" target="_blank"><img width="20" src="/public/logo.png" alt="logo"></a></p>
+</p>
+
+<h1 align="center">jk-management-ui</h1>
+
+
+## Installation
+
+jk-management-ui is available on code.aliyun.com.
+
+**[Stable channel (v0.x)]**
+```sh
+$ git clone git@code.aliyun.com:jklm/jk-management-ui.git
+$ cd jk-management-ui
+$ yarn
+$ yarn start
+```
+Enter the "http://127.0.0.1:8080" in the browser.
+
+## Documentation
+
+Check out our [documentation website](/).
+
+## Contributing
+
+We'd greatly appreciate any [contribution](/CONTRIBUTING.md) you make.)
+
+## Changelog
+
+Recently Updated?
+Please read the [changelog](/CHANGELOG.md).
+
+## Roadmap
+
+The future plans and high priority features and enhancements can be found in the [ROADMAP.md](/ROADMAP.md) file.
+
+## Thanks
+
+[<img src="https://raw.githubusercontent.com/6thquake/react-material/develop/static/icons/150x150.png" height="28"> React-Material](https://github.com/6thquake/react-material)
+
+Thank you to [React-Material](https://github.com/6thquake/react-material) for providing the awesome ui components.
+
+[<img src="https://www.browserstack.com/images/mail/browserstack-logo-footer.png" width="120">](https://www.browserstack.com/)
+
+Thank you to [BrowserStack](https://www.browserstack.com/) for providing the infrastructure that allows us to test in real browsers.
+
+## License
+
+This project is licensed under the terms of the
+[MIT license](/LICENSE).
---
+++ config-overrides.js
@@ -0,0 +1,49 @@
+const {
+    injectBabelPlugin
+} = require('react-app-rewired');
+const paths = require('react-scripts/config/paths');
+const path = require('path');
+const fs = require('fs');
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+    webpack: function (config, env) {
+        if(env==='production'){
+            config.output.path = paths.appBuild = resolveApp('../build');
+        }
+        config = injectBabelPlugin(['import', {
+            libraryName: '@6thquake/react-material',
+            libraryDirectory: 'es',
+            style: 'css'
+        }], config);
+
+        config = injectBabelPlugin(['syntax-dynamic-import'], config);
+        const alias = {
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@config': path.resolve(__dirname, 'src/config')
+        };
+        for(const k in alias){
+            config.resolve.alias[k] = alias[k];
+        }
+        return config;
+    },
+
+    devServer: function (configFunction) {
+        return function (proxy, allowedHost) {
+            const config = configFunction(proxy, allowedHost);
+            config.proxy = {
+                "/api": {
+                    "target": "/",
+                    "headers": {
+                    },
+                    "changeOrigin": true,
+                    "secure": false
+                }
+            };
+
+            return config;
+        }
+    }
+};
